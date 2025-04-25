c495b5aa0bcb8e0989f39f9c3eca81c956a61d7c
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-04-25 17:53:05 +0200

    update config

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,12 @@
 import css from './ImageGallery.module.css';
 import ImageCard from '../ImageCard/ImageCard';
 
-export default function ImageGallery({ images, onImageClick }) {
+export default function ImageGallery({ images, onCardClick }) {
   return (
     <ul className={css.gallery}>
       {images.map(image => (
         <li key={image.id} className={css.galleryItem}>
-          <ImageCard image={image} onClick={() => onImageClick(image)} />
+          <ImageCard image={image} onClick={() => onCardClick(image)} />
         </li>
       ))}
     </ul>
